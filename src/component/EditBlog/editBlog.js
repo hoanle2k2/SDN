0a60567ea8e8404d4089b5675266ca9ff3106163
DucHM <starnet.tom02@gmail.com>
2023-10-17 14:31:43 +0700

    Update create blog

--- src/component/EditBlog/editBlog.js
+++ src/component/EditBlog/editBlog.js
@@ -1,5 +1,8 @@
 import { React, useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
 import './EditBlog.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,14 +11,21 @@ const EditBlog = () => {
         margin: '10px',
         padding: '5px',
     };
+
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const handleEditorChange = (state) => {
+        setEditorState(state);
+    };
+
+
     return (
         <>
             <div class="row">
                 <div class="col-md-10 offset-md-1 col-xs-12">
                     <Formik
-                        // initialValues={initialValues}
-                        // onSubmit={handleSubmit}
-                        // validate={validateForm}
+                    // initialValues={initialValues}
+                    // onSubmit={handleSubmit}
+                    // validate={validateForm}
                     >
                         <Form style={fieldStyle}>
                             <div className='my-2'>
@@ -23,28 +33,38 @@ const EditBlog = () => {
                                 <h5>    <ErrorMessage style={{ color: 'red' }} name="title" component="div" /></h5>
                             </div>
                             <div className='my-2'>
-                                <Field type="text" placeholder="What's this blog about?" name="description" className="form-control form-control-lg" />
-                                <h5><ErrorMessage style={{ color: 'red' }} name="description" component="div" /></h5>
+                                {/* <Field
+                                    as="textarea"
+                                    rows="8"
+                                    placeholder="Write your blog (in markdown)"
+                                    name="body"
+                                    className="form-control form-control-lg"
+                                    
+                                /> */}
+                                <Editor
+                                    editorState={editorState}
+                                    onEditorStateChange={handleEditorChange}
+                                />
+                                <h5><ErrorMessage style={{ color: 'red' }} name="body" component="div" /></h5>
                             </div>
-                            <div className='my-2'>
-                                <Field as="textarea" rows="8" placeholder="Write your blog (in markdown)" name="body" className="form-control form-control-lg" />
-                                <h5>   <ErrorMessage style={{ color: 'red' }} name="body" component="div" /></h5>
+                            <div className="my-2">
+                                
                             </div>
                             <div className='my-2'>
                                 <FieldArray name="tagList">
+                                    <div>
                                         <div>
-                                                <div>
-                                                    <Field
-                                                        // name={`tagList[${index}]`}
-                                                        placeholder="Enter tags"
-                                                        className="form-control form-control-lg"
-                                                    />
-                                                </div>
-
-                                            <button className='btn btn-info' type="button" >
-                                                Add Tag
-                                            </button>
+                                            <Field
+                                                // name={`tagList[${index}]`}
+                                                placeholder="Enter tags"
+                                                className="form-control form-control-lg"
+                                            />
                                         </div>
+
+                                        <button className='btn btn-info' type="button" >
+                                            Add Tag
+                                        </button>
+                                    </div>
 
                                 </FieldArray>
                             </div>
