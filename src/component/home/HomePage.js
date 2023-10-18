73b5cf8c5a221d78f2780890bb99946b25a5233f
hoanle2k2 <hlh2k2hlh@gmail.com>
2023-10-18 16:52:08 +0700

    Update home

--- src/component/home/HomePage.js
+++ src/component/home/HomePage.js
@@ -6,17 +6,28 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
-import { Link } from "react-router-dom";
+import { Await, Link } from "react-router-dom";
 import { format } from "date-fns";
 import './Homepage.css'
 
 const HomePage = () => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [currentSort, setCurrentSort] = useState(1);
+   const [orderBy, setOrderBy] = useState(-1)
+
+
     const [articles, setArticles] = useState([]);
     const [articlesCount, setArticlesCount] = useState(0);
     const limit = 10;
 
     const [tab, setTab] = useState(1);
+    
+     const [tag, setTag] = useState([]);
+    const setTablist=(arr)=>{
+        if(!tag.includes(arr))
+            setTag([...tag,arr])
+    }
+    const [sort, setSort] = useState(1);
 
     useEffect(() => {
         const fetchArticles = async () => {
@@ -27,14 +38,27 @@ const HomePage = () => {
                 const response = await axios.get(apiUrl);
 
                 setArticles(response.data.articles);
-                setArticlesCount(response.data.articlesCount);
+                setArticlesCount(response.date.articlesCount);
+                
             } catch (error) {
                 console.error("Error fetching articles:", error);
             }
+            
         };
 
         fetchArticles();
-    }, [currentPage, limit]);
+    }, [currentPage, limit, currentSort,tag]);
+    console.log("Done")
+
+    // function sortData(data) {
+        
+    //     if(tag.length!=0)
+    //     .filter((item) => sortData(item))
+    //     console.log(data)
+    //     return checker(data.tagList,tag)
+    //   }
+
+    //   const checker = (arr, target) => target.every(v => arr.includes(v));
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
@@ -46,7 +70,9 @@ const HomePage = () => {
     };
 
     const totalPages = Math.ceil(articlesCount / limit);
-
+    const removeTag = (id) => {
+        setTag(tag.filter(item=>tag.indexOf(item)!==id))
+    }
     return (
         <div className="bg-white body">
             {/* <div>
@@ -54,21 +80,21 @@ const HomePage = () => {
             </div> */}
 
             <div className="bg-white ">
-                <nav class="navbar navbar-expand-lg bg-light">
-                    <div class="container-fluid bg-w">
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active text-primary" aria-current="page" href="#"><p className="h5">Lastest</p></a>
+                <nav className="navbar navbar-expand-lg bg-light">
+                    <div className="container-fluid bg-w">
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li onClick={() => setSort(1)} className="nav-item">
+                                    <a className={`nav-link active ${sort === 1 ? 'text-primary' : 'text-dark'}`} aria-current="page" href="#"><p className="h5">Editors'choice</p></a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link  text-primary" href="#"><p className="h5">Editors'Choice</p></a>
+                                <li onClick={() => setSort(2)} class="nav-item">
+                                    <a className={`nav-link active ${sort === 2 ? 'text-primary' : 'text-dark'}`} href="#"><p className="h5">Followings</p></a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link  text-primary" href="#"><p className="h5">Hot</p></a>
+                                <li onClick={() => setSort(3)} class="nav-item">
+                                    <a className={`nav-link active ${sort === 3 ? 'text-primary' : 'text-dark'}`} href="#"><p className="h5">Hot</p></a>
                                 </li>
                             </ul>
-                            <button class="btn  btn-outline-primary " type="submit"><Link className="text-decoration-none" to={`/edit`}>CREATE POST</Link></button>
+                            <button className="btn  btn-outline-primary " type="submit"><Link className="text-decoration-none" to={`/edit`}>CREATE POST</Link></button>
 
                         </div>
                     </div>
@@ -78,14 +104,44 @@ const HomePage = () => {
 
             <div className="content d-flex">
                 <div className='aricle col-9'>
+
+
+                    <div class="dropdown">
+                        <span className="h6 text-secondary ">Sort by: </span>
+                        <span className="dropdown-toggle text-primary h6" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown">
+
+                            {currentSort === 1 && 'Date'}
+                            {currentSort === 2 && 'View'}
+                            {currentSort === 3 && 'Voted'}
+                            {currentSort === 4 && 'Bookmarked'}
+                        </span>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li onClick={() => setCurrentSort(1)}><a class="dropdown-item" >Date</a></li>
+                            <li onClick={() => setCurrentSort(2)}><a class="dropdown-item" >View</a></li>
+                            <li onClick={() => setCurrentSort(3)}><a class="dropdown-item" >Voted</a></li>
+                            <li onClick={() => setCurrentSort(4)}><a class="dropdown-item" >Bookmarked</a></li>
+                        </ul>
+                    </div>
+                    <ul className='tag_list'>
+                        {tag.map((tagList, index) => (
+                            <Link><li className="tag" key={tagList}>{tagList} <i onClick={()=>removeTag(index)} class="bi bi-x-circle text-danger"></i></li></Link>
+
+                        ))
+                        }
+                    </ul>
+
                     <div >
                         <div className="d-flex justify-content-end">
-                            <Link to={`/`} onClick={() => setTab(1)}> <i type="button" class={`icon1 bi bi-card-list ${tab === 2 && 'text-black'}`} data-toggle="tooltip" data-placement="bottom" title="Only title"></i></Link>
-                            <Link to={`/`} onClick={() => setTab(2)}> <i type="button" class={`icon1 bi bi-newspaper ${tab === 1 && 'text-black'}`} data-placement="bottom" title="With preview contents"></i></Link>
+                            <Link to={`/`} onClick={() => setOrderBy(orderBy*(-1))}> <i type="button" className={`${orderBy === -1? 'bi bi-arrow-down':'bi bi-arrow-up'}`} data-toggle="tooltip" data-placement="bottom" title="Only title"></i></Link>
+                            <Link to={`/`} onClick={() => setTab(1)}> <i type="button" className={`icon1 bi bi-card-list ${tab === 2 && 'text-black'}`} data-toggle="tooltip" data-placement="bottom" title="Only title"></i></Link>
+                            <Link to={`/`} onClick={() => setTab(2)}> <i type="button" className={`icon1 bi bi-newspaper ${tab === 1 && 'text-black'}`} data-placement="bottom" title="With preview contents"></i></Link>
                         </div>
 
                         {articles.map(article => (
+                            
+                           
                             <div className='article d-flex' key={article.slug}>
+                               
                                 <div className="col-1">
                                     <img className="author_avatar img-fluid rounded-circle" src={article.author.image} alt="avatar" />
                                 </div>
@@ -98,7 +154,7 @@ const HomePage = () => {
                                     {tab === 2 && <p className='text-truncate'>{article.description}</p>}
                                     <ul className='tag_list'>
                                         {article.tagList.map(tagList => (
-                                            <Link to={`/`}><li className="tag" key={tagList}>{tagList}</li></Link>
+                                            <Link><li onClick={() => setTablist(tagList)} className="tag" key={tagList}>{tagList}</li></Link>
                                         ))
                                         }
                                     </ul>
@@ -107,6 +163,7 @@ const HomePage = () => {
                                         <i class="emotion bi bi-bookmark"> 5</i>
                                         <i class="emotion bi bi-chat"> 5</i>
                                         <i class="emotion bi bi-heart-fill"> 5</i>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -120,6 +177,9 @@ const HomePage = () => {
                         />
                     </div>
                 </div>
+
+                {/* author section */}
+
                 <div className="author col-3 ">
                     <div >
                         <p className=" text-primary h5 ">TOP AUTHORS</p>
