2bcc30e40d11654c074845ae58d1df94af86eda5
manh <congmanhbn20021@gmail.com>
2023-10-19 19:21:00 +0700

    complete register

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import BlogList from "./component/blogList/BlogList";
 import EditBlog from "./component/editBlog/EditBlog";
 import HomePage from "./component/home/HomePage";
 import Header from "./component/header/Header";
-import Verify from "./component/register/Verify";
 
 function App() {
   const location = useLocation();
@@ -22,7 +21,6 @@ function App() {
           <Route path="/login" element={<SignInSide />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/forgot" element={<ForgotPassword />} />
-          <Route path="/verify" element={<Verify />} />
           <Route path="/blogl" element={<BlogList />} />
           <Route path="/edit" element={<EditBlog />} />
         </Routes>
