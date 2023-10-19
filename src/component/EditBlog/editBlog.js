3818c50140e939f0286bfacd78b36221d8aff7ef
DucHM <starnet.tom02@gmail.com>
2023-10-19 11:40:22 +0700

    Update Edit

--- src/component/EditBlog/editBlog.js
+++ src/component/EditBlog/editBlog.js
@@ -13,27 +13,79 @@ const EditBlog = () => {
     };
 
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
-    const handleEditorChange = (state) => {
+    // const handleEditorChange = (state) => {
+    //     setEditorState(state);
+    // };
+    const handleEditorChange = (state, formik) => {
         setEditorState(state);
+        if (formik) {
+            formik.setFieldValue("Content", state.getCurrentContent().getPlainText());
+        }
+    };
+
+    const validateForm = (values) => {
+        const errors = {};
+
+        if (!values.Title) {
+            errors.Title = 'Title can not be blank.';
+        }
+        else if (!values.Content) {
+            errors.Content = ' Content can not be blank..';
+        }
+        else if (!values.TopicID) {
+            errors.TopicID = ' TopicID can not be blank.';
+        }
+
+        return errors;
+    };
+
+    const initialValues = {
+        Title: "",
+        Content: "",
+        TopicID :"",
+        // body: "",
+        // tagList: [
+
+        // ]
+    };
+
+    const handleSubmit = async (values) => {
+        // const data = {
+        //     article: {
+        //         ...values
+        //     }
+        // }
+        console.log(values);
+        // Xử lý logic khi form được submit
+        // console.log("form", data);
+        // try {
+        //     const res = await postNewArticle(token, data)
+        //     console.log("res from form", res);
+        //     const dataRespon = res?.data?.article
+        //     navigate(`/article/${dataRespon.slug}`)
+        // } catch (error) {
+        //     console.log(error);
+        // }
     };
 
 
     return (
-        <>
-            <div class="row">
+        <div className='mt-26'>
+            <div className="row">
                 <div class="col-md-10 offset-md-1 col-xs-12">
                     <Formik
-                    // initialValues={initialValues}
-                    // onSubmit={handleSubmit}
-                    // validate={validateForm}
+                        initialValues={initialValues}
+                        onSubmit={handleSubmit}
+                        validate={validateForm}
                     >
-                        <Form style={fieldStyle}>
-                            <div className='my-2'>
-                                <Field type="text" placeholder="Blog Title" name="title" className="form-control form-control-lg" />
-                                <h5>    <ErrorMessage style={{ color: 'red' }} name="title" component="div" /></h5>
-                            </div>
-                            <div className='my-2'>
-                                {/* <Field
+                        {(formik) => (
+                            <Form style={fieldStyle}>
+                                <div className='my-2'>
+                                    <Field type="text" placeholder="Blog Title" name="Title" className="form-control form-control-lg" />
+                                    <h5> <ErrorMessage style={{ color: 'red' }} name="Title" component="div" /></h5>
+                                </div>
+                                <div className='my-2 form-control form-control-lg'>
+                                    {/* <Field
                                     as="textarea"
                                     rows="8"
                                     placeholder="Write your blog (in markdown)"
@@ -41,16 +93,30 @@ const EditBlog = () => {
                                     className="form-control form-control-lg"
                                     
                                 /> */}
-                                <Editor
+
+                                    {/* <Editor
                                     editorState={editorState}
                                     onEditorStateChange={handleEditorChange}
-                                />
-                                <h5><ErrorMessage style={{ color: 'red' }} name="body" component="div" /></h5>
-                            </div>
-                            <div className="my-2">
-                                
-                            </div>
-                            <div className='my-2'>
+                                    placeholder="Write your blog (in markdown)"
+                                   
+                                /> */}
+                                    <Editor
+                                        editorState={editorState}
+                                        onEditorStateChange={(state) => handleEditorChange(state, formik)}
+                                        placeholder="Write your blog (in markdown)"
+                                    />
+                                    <h5><ErrorMessage style={{ color: 'red' }} name="body" component="div" /></h5>
+                                </div>
+                                <div className="my-2">
+                                    <Field as="select" id="selectedOption" name="TopicID">
+                                        <option value="">TopicID</option>
+                                        <option value="option1">Option 1</option>
+                                        <option value="option2">Option 2</option>
+                                        <option value="option3">Option 3</option>
+                                    </Field>
+                                </div>
+
+                                {/* <div className='my-2'>
                                 <FieldArray name="tagList">
                                     <div>
                                         <div>
@@ -67,15 +133,16 @@ const EditBlog = () => {
                                     </div>
 
                                 </FieldArray>
-                            </div>
-                            <div className="d-flex justify-content-end">
-                                <button className=" sb btn btn-success btn-lg" type="submit">Submit</button>
-                            </div>
-                        </Form>
+                            </div> */}
+                                <div className="d-flex justify-content-end">
+                                    <button className=" sb btn btn-success btn-lg" type="submit">Submit</button>
+                                </div>
+                            </Form>
+                        )}
                     </Formik>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
