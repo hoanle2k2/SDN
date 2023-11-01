9fb49a256a4ffff5cd26558b9c880a29b30ce870
ThangPQ <phamthangyn@gmail.com>
2023-11-01 09:54:36 +0700

    local

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './Header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showProfileList, setShowProfileList] = useState(false);
-  const [userRole, setUserRole] = useState('');
-  const name = localStorage.getItem('name');
+  const [userRole, setUserRole] = useState("");
+  const name = localStorage.getItem("name");
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('accessToken');
-    const storedUserRole = localStorage.getItem('Role');
+    const storedToken = localStorage.getItem("accessToken");
+    const storedUserRole = localStorage.getItem("Role");
 
     if (storedToken) {
       setLoggedIn(true);
       setUserRole(storedUserRole);
     } else {
       setLoggedIn(false);
-      setUserRole('');
+      setUserRole("");
     }
   }, []);
 
@@ -28,19 +28,24 @@ const Header = () => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('Role');
-    localStorage.removeItem('name');
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("Role");
+    localStorage.removeItem("name");
 
-    setUserRole('');
+    setUserRole("");
     setLoggedIn(false);
   };
 
   return (
-    <header className={`header ${loggedIn ? 'logged-in' : 'guest'}`}>
+    <header className={`header ${loggedIn ? "logged-in" : "guest"}`}>
       <div className="logo">
-        <Link to='/'>
-          <img className='logo_img' src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" alt="Logo" style={{width : '150px' , objectFit : 'cover'}}/>
+        <Link to="/">
+          <img
+            className="logo_img"
+            src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png"
+            alt="Logo"
+            style={{ width: "150px", objectFit: "cover" }}
+          />
         </Link>
       </div>
 
@@ -60,32 +65,31 @@ const Header = () => {
 
       {loggedIn && (
         <div className="user-actions">
-          {userRole === 'USER' && (
+          {userRole === "USER" && (
             <div>
               <Link to="/edit">Tạo bài viết</Link>
             </div>
           )}
 
-          {userRole === 'MANAGER' && (
+          {userRole === "MANAGER" && (
             <div>
               <span>Duyệt bài</span>
             </div>
           )}
 
           <div className="user-profile">
-            <img src="https://inkythuatso.com/uploads/thumbnails/800/2021/11/logo-fpt-inkythuatso-1-01-01-14-33-35.jpg" alt="Avatar" />
-            <span>Xin chào {name.toUpperCase()}</span>
-            <FontAwesomeIcon
-              icon={faBars}
-              className="navbar"
-              onClick={toggleProfileList}
+            <img
+              src="https://inkythuatso.com/uploads/thumbnails/800/2021/11/logo-fpt-inkythuatso-1-01-01-14-33-35.jpg"
+              alt="Avatar"
             />
+            <span>Xin chào {name.toUpperCase()}</span>
+            <FontAwesomeIcon icon={faBars} className="navbar" onClick={toggleProfileList} />
           </div>
 
           {showProfileList && (
             <div className="profile-list">
-              {userRole === 'ADMIN' && (
-                <div className='narbar-list'>
+              {userRole === "ADMIN" && (
+                <div className="narbar-list">
                   <div>Dashboard</div>
                   <div>Quản lý người dùng</div>
                   <div>Danh sách các đề tài</div>
@@ -93,16 +97,18 @@ const Header = () => {
                 </div>
               )}
 
-              {userRole === 'MANAGER' && (
-                <div className='narbar-list'>
+              {userRole === "MANAGER" && (
+                <div className="narbar-list">
                   <div>Danh sách các đề tài</div>
                   <div>Danh sách bài viết bị báo cáo</div>
                 </div>
               )}
 
-              {userRole === 'USER' && (
-                <div className='narbar-list'>
-                  <div className='profile'>Trang cá nhân</div>
+              {userRole === "USER" && (
+                <div className="narbar-list">
+                  <div className="profile">
+                    <Link to="/users">Trang cá nhân</Link>
+                  </div>
                   <div onClick={handleLogout}>Đăng xuất</div>
                 </div>
               )}
